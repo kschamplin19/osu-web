3f624af16f1e634a6785d30d0aaba377438dbfbb
Saji Champlin <kschamplin19@blakeschool.org>
2016-05-12 07:52:58 -0400

    Applied fixes from StyleCI

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -1,52 +1,54 @@
 <?php
 
-/**
- *    Copyright 2015 ppy Pty. Ltd.
- *
- *    This file is part of osu!web. osu!web is distributed with the hope of
- *    attracting more community contributions to the core ecosystem of osu!.
- *
- *    osu!web is free software: you can redistribute it and/or modify
- *    it under the terms of the Affero GNU General Public License version 3
- *    as published by the Free Software Foundation.
- *
- *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
- *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *    See the GNU Affero General Public License for more details.
- *
- *    You should have received a copy of the GNU Affero General Public License
- *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
- */
- namespace App\Models;
+ /**
+  *    Copyright 2015 ppy Pty. Ltd.
+  *
+  *    This file is part of osu!web. osu!web is distributed with the hope of
+  *    attracting more community contributions to the core ecosystem of osu!.
+  *
+  *    osu!web is free software: you can redistribute it and/or modify
+  *    it under the terms of the Affero GNU General Public License version 3
+  *    as published by the Free Software Foundation.
+  *
+  *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+  *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  *    See the GNU Affero General Public License for more details.
+  *
+  *    You should have received a copy of the GNU Affero General Public License
+  *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+  */
+namespace App\Models;
 
- use Carbon\Carbon;
- use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;
 
  class Team extends Model
  {
-    protected $table = 'teams';
-    protected $primaryKey = 'id';
-    protected $visible = ['name'];
-    protected $guarded = [];
-    protected $casts = [
+     protected $table = 'teams';
+     protected $primaryKey = 'id';
+     protected $visible = ['name'];
+     protected $guarded = [];
+     protected $casts = [
       'id' => 'integer',
       'name' => 'string',
     ];
-    public static function getAdmins()
-    {
-      return [1]; #temporary user 1
-    }
-    public static function getMembers()
-    {
-      return [2]; #temporary user 2
-    }
-    public static function lookup($teamname_or_id, $lookup_type = null, $find_all = false)
-    {
-        if (!present($teamname_or_id)) {
-            return;
-        }
 
-        switch ($lookup_type) {
+     public static function getAdmins()
+     {
+         return [1]; //temporary user 1
+     }
+
+     public static function getMembers()
+     {
+         return [2]; //temporary user 2
+     }
+
+     public static function lookup($teamname_or_id, $lookup_type = null, $find_all = false)
+     {
+         if (!present($teamname_or_id)) {
+             return;
+         }
+
+         switch ($lookup_type) {
             case 'string':
                 $team = self::where('name', $teamname_or_id)->orWhere('name_clean', $teamname_or_id);
                 break;
@@ -64,10 +66,10 @@
                 break;
         }
 
-        if (!$find_all) {
-            $team = $team;
-        }
+         if (!$find_all) {
+             $team = $team;
+         }
 
-        return $team->first();
-    }
+         return $team->first();
+     }
  }
